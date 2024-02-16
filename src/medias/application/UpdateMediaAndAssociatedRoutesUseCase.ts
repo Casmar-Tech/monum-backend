e23fccc0b792bedd980013e41a1ba998a1cef9b0
Xavier Huix Trenco <xeviht@MacBook-MacBook-Pro-de-Xavier.local>
2024-02-16 11:17:50 +0100

    update medias not routes

--- src/medias/application/UpdateMediaAndAssociatedRoutesUseCase.ts
+++ src/medias/application/UpdateMediaAndAssociatedRoutesUseCase.ts
@@ -1,8 +1,5 @@
-import { MongoRouteModel } from "../../routes/infrastructure/mongoModel/MongoRouteModel.js";
 import { MongoMediaModel } from "../infrastructure/mongoModel/MongoMediaModel.js";
 import { IMedia } from "../domain/interfaces/IMedia.js";
-import { getTrip } from "../../routes/infrastructure/osrm/GetTrip.js";
-import { getRoute } from "../../routes/infrastructure/osrm/GetRoute.js";
 import { ApolloError } from "apollo-server-errors";
 import { MongoPlaceModel } from "../../places/infrastructure/mongoModel/MongoPlaceModel.js";
 
@@ -19,35 +16,6 @@ export default async function UpdateMediaAndAssociatedRoutesUseCase(
     const place = await MongoPlaceModel.findById(mediaUpdated.placeId);
     if (!place) {
       throw new ApolloError("Place not found", "PLACE_NOT_FOUND");
-    }
-    const routesToUpdate = await MongoRouteModel.find({
-      "stops.media._id": id,
-    });
-    for (const route of routesToUpdate) {
-      const stopIndex = route.stops.findIndex(
-        (stop) => stop.media._id?.toString() === id
-      );
-      if (stopIndex > -1) {
-        route.stops[stopIndex].media = mediaUpdated;
-        const coordinates = route.stops
-          .map((stop) => [
-            place.address.coordinates.lng,
-            place.address.coordinates.lat,
-          ])
-          .filter(Boolean) as [number, number][];
-        const tripData = await getTrip("foot", coordinates);
-        const routeData = await getRoute("foot", coordinates);
-        route.stops = route.stops.map((stop, index) => ({
-          media: stop.media,
-          order: index,
-          optimizedOrder: tripData.waypoints[index].waypoint_index,
-        }));
-        route.duration = routeData.routes[0].duration;
-        route.optimizedDuration = tripData.trips[0].duration;
-        route.distance = routeData.routes[0].distance;
-        route.optimizedDistance = tripData.trips[0].distance;
-        route.save();
-      }
     }
     return mediaUpdated;
   }
