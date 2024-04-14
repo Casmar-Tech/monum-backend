9855339e659588811b419bb93ccde75854473987
Pau Vilella <pau.vilella.st@gmail.com>
2024-04-14 18:43:29 +0200

    Add delete media functionality

--- src/medias/application/DeleteMediaAndUpdatedAssociatedRoutesUseCase.ts
+++ src/medias/application/DeleteMediaAndUpdatedAssociatedRoutesUseCase.ts
@@ -1,18 +1,19 @@
 import { ApolloError } from "apollo-server-errors";
+import { MongoMediaModel } from "../infrastructure/mongoModel/MongoMediaModel.js";
 
 export default async function DeleteMediaAndUpdatedAssociatedRoutesUseCase(
   id: string
 ): Promise<boolean> {
   try {
+    const filter = { _id: id };
+    const update = { deleted: true };
+    await MongoMediaModel.findOneAndUpdate(filter, update, {
+      new: true, // return the updated document
+      useFindAndModify: false, // to use MongoDB driver's findOneAndUpdate() instead of findAndModify()
+    });
     return true;
   } catch (error) {
-    console.error(
-      "Error while deleting Media and updating associated Routes",
-      error
-    );
-    throw new ApolloError(
-      "Error while deleting Media and deleting associated Routes",
-      "DELETE_MEDIA_AND_UPDATE_ASSOCIATED_ROUTES"
-    );
+    console.error("Error while deleting Media.", error);
+    throw new ApolloError("Error while deleting Media", "DELETE_MEDIA_ERROR");
   }
 }
